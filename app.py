aaae28ea7f3b95edcb993f28dc9e398694ca853d
Frederic MASSON DELAS <39305682-fredericmassond@users.noreply.replit.com>
2025-02-04 00:06:34 +0000

    Modification de app.py pour mettre à jour render

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    return render_template('index.html')  # Modifier ici pour charger index.html
+@app.route('/')
+def home():
+    return render_template('form.html')
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
